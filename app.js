a79dfd0940629a82b3d0ddf934d340cf110fb7ca
Lucas Holmquist <lholmqui@redhat.com>
2017-07-31 14:56:52 -0400

    chore: process updates
    * add eslint
    
    * License file added
    
    * adding in travis
    
    * test coverage added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 
-var app = express();
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
