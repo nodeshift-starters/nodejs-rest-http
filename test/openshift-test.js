8d61dc971e8a39accb881a5c754cefad52305ef6
mocenas <mocenas@redhat.com>
2018-01-30 14:53:34 +0100

    Fixed codestyle

--- test/openshift-test.js
+++ test/openshift-test.js
@@ -1,76 +1,69 @@
 const test = require('tape');
 const openshiftTestAssistant = require('openshift-test-assistant');
-
+const path = require('path');
 const request = require('supertest');
 
-test('setup', function (t) {
-    openshiftTestAssistant.deploy({
-        "projectLocation" : __dirname + "/..",
-        "strictSSL" : false
-    }).then(() => {
-        t.ok(true); //indicate success
-        t.end();
-    }).catch(reason => {
-        t.fail(reason);
-        t.end();
-    });
+test('setup', (t) => {
+  openshiftTestAssistant.deploy({
+    'projectLocation': path.join(__dirname, '/..'),
+    'strictSSL': false
+  }).then(() => {
+    t.ok(true); // indicate success
+    t.end();
+  }).catch(reason => {
+    t.fail(reason);
+    t.end();
+  });
 });
 
 test('test openshift greeting with no query param', (t) => {
-    if (!openshiftTestAssistant.isReady()){
-        t.skip();
+  if (!openshiftTestAssistant.isReady()) {
+    t.skip();
+    t.end();
+  } else {
+    request(openshiftTestAssistant.getRoute())
+      .get('/api/greeting')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then(response => {
+        t.equal(response.body.content, 'Hello, World');
         t.end();
-    }
-    else {
-        request(openshiftTestAssistant.getRoute())
-            .get('/api/greeting')
-            .expect('Content-Type', /json/)
-            .expect(200)
-            .then(response => {
-                t.equal(response.body.content, 'Hello, World');
-                t.end();
-            })
-            .catch(reason => {
-                t.fail(reason);
-                t.end();
-            });
-    }
+      })
+      .catch(reason => {
+        t.fail(reason);
+        t.end();
+      });
+  }
 });
 
 test('test openshift greeting with query param', (t) => {
-    if (!openshiftTestAssistant.isReady()){
-        t.skip();
+  if (!openshiftTestAssistant.isReady()) {
+    t.skip();
+    t.end();
+  } else {
+    request(openshiftTestAssistant.getRoute())
+      .get('/api/greeting?name=Luke')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then(response => {
+        t.equal(response.body.content, 'Hello, Luke');
         t.end();
-    }
-    else {
-        request(openshiftTestAssistant.getRoute())
-            .get('/api/greeting?name=Luke')
-            .expect('Content-Type', /json/)
-            .expect(200)
-            .then(response => {
-                t.equal(response.body.content, 'Hello, Luke');
-                t.end();
-            })
-            .catch(reason => {
-                t.fail(reason);
-                t.end();
-            });
-    }
-});
-
-test('teardown', function (t){
-    openshiftTestAssistant.undeploy({
-        "projectLocation" : __dirname + "/..",
-        "strictSSL" : false
-    }).then(() => {
-        t.end();
-    }).catch(reason => {
+      })
+      .catch(reason => {
         t.fail(reason);
         t.end();
-    });
+      });
+  }
 });
 
-
-
-
-
+test('teardown', (t) => {
+  openshiftTestAssistant.undeploy({
+    'projectLocation': path.join(__dirname, '/..'),
+    'strictSSL': false
+  }).then(() => {
+    t.end();
+  }).catch(reason => {
+    t.fail(reason);
+    t.end();
+  });
+});
