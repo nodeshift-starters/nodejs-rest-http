730816886700020e3c9ca591fc528101296310e9
mocenas <mocenas@redhat.com>
2018-02-06 14:31:34 +0100

    Created openshift tests in ava framework

--- test/openshift-test.js
+++ test/openshift-test.js
@@ -1,63 +1,34 @@
-const test = require('tape');
 const OpenshiftTestAssistant = require('openshift-test-assistant');
 const openshiftAssistant = new OpenshiftTestAssistant();
 const path = require('path');
 const request = require('supertest');
+const test = require('ava');
 
-test('setup', (t) => {
-  t.plan(1);
-  openshiftAssistant.deploy({
+test.before('setup', () => {
+  return openshiftAssistant.deploy({
     'projectLocation': path.join(__dirname, '/..'),
     'strictSSL': false
-  }).then(() => {
-    t.pass('Application deployed'); // indicate success
-  }).catch(reason => {
-    t.fail(reason);
   });
 });
 
-test('test openshift greeting with no query param', (t) => {
+test('test openshift greeting with no query param', async t => {
   t.plan(1);
-  if (!openshiftAssistant.isReady()) {
-    t.skip('Application not ready, skipping the test');
-  } else {
-    request(openshiftAssistant.getRoute())
-      .get('/api/greeting')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .then(response => {
-        t.equal(response.body.content, 'Hello, World', 'Received message should math the expected one');
-      })
-      .catch(reason => {
-        t.fail(reason);
-      });
-  }
+  const response = await request(openshiftAssistant.getRoute())
+    .get('/api/greeting')
+    .expect('Content-Type', /json/)
+    .expect(200);
+  t.is(response.body.content, 'Hello, World', 'Received message should math the expected one');
 });
 
-test('test openshift greeting with query param', (t) => {
+test('test openshift greeting with query param', async t => {
   t.plan(1);
-  if (!openshiftAssistant.isReady()) {
-    t.skip('Application not ready, skipping the test');
-  } else {
-    request(openshiftAssistant.getRoute())
-      .get('/api/greeting?name=Luke')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .then(response => {
-        t.equal(response.body.content, 'Hello, Luke', 'Received message should math the expected one');
-      })
-      .catch(reason => {
-        t.fail(reason);
-      });
-  }
+  const response = await request(openshiftAssistant.getRoute())
+    .get('/api/greeting?name=Luke')
+    .expect('Content-Type', /json/)
+    .expect(200);
+  t.is(response.body.content, 'Hello, Luke', 'Received message should math the expected one');
 });
 
-test('teardown', (t) => {
-  t.plan(1);
-  openshiftAssistant.undeploy()
-    .then(() => {
-      t.pass('Application successfully undeployed');
-    }).catch(reason => {
-      t.fail(reason);
-    });
+test.after('teardown', () => {
+  return openshiftAssistant.undeploy();
 });
