69134279ae9cf8fb151255b889d91e2c709c2731
Lucas Holmquist <lholmqui@redhat.com>
2018-04-03 00:34:15 +0000

    chore(lint): moving to xo for linting.  fixes #94

--- app.js
+++ app.js
@@ -18,16 +18,16 @@
  *
  */
 
-const express = require('express');
 const path = require('path');
+const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/', express.static(path.join(__dirname, 'public')));
-// expose the license.html at http[s]://[host]:[port]/licences/licenses.html
+// Expose the license.html at http[s]://[host]:[port]/licences/licenses.html
 app.use('/licenses', express.static(path.join(__dirname, 'licenses')));
 
 app.use('/api/greeting', (request, response) => {
