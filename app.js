c09d0b4fbd14b852f7c52e505eaa332873635658
Helio Frota <00hf11@gmail.com>
2021-12-08 17:36:03 -0300

    chore: updates
    * license-reporter and rhoaster removed
    * dependencies updates

--- app.js
+++ app.js
@@ -27,8 +27,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', express.static(path.join(__dirname, 'public')));
-// Expose the license.html at http[s]://[host]:[port]/licences/licenses.html
-app.use('/licenses', express.static(path.join(__dirname, 'licenses')));
 
 app.use('/api/greeting', (request, response) => {
   const name = request.query ? request.query.name : undefined;
