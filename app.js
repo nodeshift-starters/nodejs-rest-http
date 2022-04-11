6453d11ebc140e6bef8757dce255e1a3527fb72f
jaffe9 <jaffar.meeran@gmail.com>
2022-04-11 11:40:34 +0300

    Test

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use('/api/greeting', (request, response) => {
   const name = request.query ? request.query.name : undefined;
-  response.send({ content: `Hello, ${name || 'World!'}` });
+  response.send({ content: `Hello Mr., ${name || 'World!'}` });
 });
 
 module.exports = app;
